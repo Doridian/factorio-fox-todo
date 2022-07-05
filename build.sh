e2c532726fdcab0722d4ec8a503e6c5b0aa94833
Doridian <git@doridian.net>
2022-07-05 12:31:11 -0700

    zip instead with tmpdir

--- build.sh
+++ build.sh
@@ -11,11 +11,22 @@ then
     VERSION="99.99.99"
 fi
 
+PROJECT_DIR="$(pwd)"
+
 sed "s~99\\.99\\.98~$VERSION~" -i info.json
 sed "s~M.version = .*$~M.version = \"$VERSION\"~" -i config.lua
 
-mkdir -p dist/zipsrc
-ln -s . dist/zipsrc/fox-todo
-cd dist/zipsrc
-zip "../fox-todo_$VERSION.zip" -r './fox-todo' -x './fox-todo/dist' -x './fox-todo/dist/*'
-cd ../..
+TMP_DIR="$(mktemp -d)"
+
+mkdir -p dist
+DIST_DIR="$PROJECT_DIR/dist"
+
+
+rm -f "$DIST_DIR/fox-todo_$VERSION.zip"
+
+ln -s "$PROJECT_DIR" "$TMP_DIR/fox-todo"
+cd "$TMP_DIR"
+zip "$DIST_DIR/fox-todo_$VERSION.zip" -v -r './fox-todo' -x './fox-todo/.gitignore' -x './fox-todo/.git' -x './fox-todo/.git/*' -x './fox-todo/.github' -x './fox-todo/.github/*'
+cd "$PROJECT_DIR"
+
+rm -rf "$TMP_DIR"
--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 -- Filled in by build process
-M.version = nil
+M.version = "0.1.0"
 
 M.tag_prefix = "TODO: "
 M.tag_prefix_len = M.tag_prefix:len()
