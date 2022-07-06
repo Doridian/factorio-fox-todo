dedbfc1f829b4cff371790769ab86c04bafb7f68
Doridian <git@doridian.net>
2022-07-06 08:29:24 -0700

    Skip init code when already up to date

--- initializer.lua
+++ initializer.lua
@@ -1,12 +1,19 @@
+local config = require("config")
 local tags = require("tags")
 
 local M = {}
 
 function M.initialize_mod()
+    if config.version and (global.mod_version == config.version) then
+        return
+    end
+
     global.player_gui = global.player_gui or {}
     global.all_todo_tags_by_force = global.all_todo_tags_by_force or {}
 
     tags.find_all_tags()
+
+    global.mod_version = config.version
 end
 
 script.on_init(M.initialize_mod)
